604f0cbc6735b7da2d49ee52820abbc096bbc707
mbfx <mbfx@mail.ru>
2019-12-15 13:06:08 +0200

    lab01 refinement

--- lab01/clean_method/shellprov.sh
+++ lab01/clean_method/shellprov.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 cd $HOME
-sudo yum -y install wget
+sudo yum -y -q install wget
 wget https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.19.36.tar.xz --directory-prefix=$HOME --quiet
 tar xf $HOME/linux-4.19.36.tar.xz --directory=$HOME
 cp /boot/config-`uname -r` $HOME/linux-4.19.36/.config
-sudo yum -y install gcc bison flex elfutils-libelf-devel bc openssl-devel perl rpm-build
+sudo yum -y -q install gcc bison flex elfutils-libelf-devel bc openssl-devel perl rpm-build
 cd $HOME/linux-4.19.36
 yes "" | make oldconfig 1>/dev/null
 make -j$((`nproc`+1)) rpm-pkg 1>/dev/null
@@ -13,4 +13,3 @@ sudo rpm -ivh --quiet $HOME/rpmbuild/RPMS/x86_64/kernel-devel-4.19.36-1.x86_64.r
 sudo rpm -iUh --quiet $HOME/rpmbuild/RPMS/x86_64/kernel-headers-4.19.36-1.x86_64.rpm
 sudo rpm -ivh --quiet $HOME/rpmbuild/SRPMS/kernel-4.19.36-1.src.rpm
 sudo grub2-set-default 0
-
